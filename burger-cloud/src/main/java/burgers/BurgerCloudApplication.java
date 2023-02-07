3201b332a623b5c5f6479a25c9bd5a60fda2c045
Egor Chervonikov <crusaderworkvii@gmail.com>
2023-02-07 21:52:39 +0300

    Created a project.

---
+++ burger-cloud/src/main/java/burgers/BurgerCloudApplication.java
@@ -0,0 +1,13 @@
+package burgers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerCloudApplication.class, args);
+	}
+
+}
---
+++ burger-cloud/src/test/java/burgers/BurgerCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package burgers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
