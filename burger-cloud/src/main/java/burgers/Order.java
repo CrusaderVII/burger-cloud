3b4b6c383ee7e98a85a4d8f00aadc5c1fb1060b1
Egor Chervonikov <crusaderworkvii@gmail.com>
2023-02-28 00:42:35 +0300

    Added fields for credit card data to cart template

--- burger-cloud/src/main/java/burgers/Order.java
+++ burger-cloud/src/main/java/burgers/Order.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.validator.constraints.CreditCardNumber;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,7 +16,9 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SecondaryTable;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -39,9 +43,21 @@ public class Order {
 	@Column(name = "date", nullable = false)
 	private LocalDateTime ordersDate;
 	
-	@NotNull(message="Please enter addres, we dont wanna send our burgers to eternal void:(")
-	@Column(name = "addres", nullable = false)
-	private String addres;
+	@NotBlank(message="Please enter addres, we dont wanna send our burgers to eternal void:(")
+	@Column(name = "address", nullable = false)
+	private String address;
+	
+	@Transient
+	@CreditCardNumber
+	private String ccNumber;
+	
+	@Transient
+	@Pattern(regexp="^(0[1-9]|1[0-2])/(20(2[3-9]|[3-9]\\d))$")
+	private String ccExperaion;
+	
+	@Transient
+	@Pattern(regexp="^\\d\\d\\d$")
+	private String ccCVV;
 	
 	@Transient
 	List<String> cart = new ArrayList<>();
@@ -56,10 +72,19 @@ public class Order {
 		this.cart.add(codeName);
 	}
 	
+	public void setCartFull(List<String> cart) {
+
+		this.cart = cart;
+	}
+	
 	public double getPrice() {
 		String res = String.format("%.2f",this.price);
 		res = res.replaceAll(",", "\\.");
 		return Double.parseDouble(res);
+	}
+	
+	public String getOrdersDate() {
+		return this.ordersDate.getDayOfMonth()+"."+this.ordersDate.getMonthValue()+"."+this.ordersDate.getYear();
 	}
 	
 	
--- burger-cloud/src/main/java/burgers/web/order/OrderController.java
+++ burger-cloud/src/main/java/burgers/web/order/OrderController.java
@@ -2,7 +2,10 @@ package burgers.web.order;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,8 +13,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,14 +30,16 @@ import burgers.data.OrderRepository;
 import burgers.data.ReferenceRepository;
 import burgers.data.UserRepository;
 import burgers.security.UserLogin;
+import freemarker.ext.beans.EnumerationModel;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 
 @RestController
 @RequestMapping("/order")
-@SessionAttributes({"order", "user"})
+@SessionAttributes({"order", "user", "orderedBurgers"})
 @Slf4j
 public class OrderController {
 	
@@ -54,14 +61,37 @@ public class OrderController {
 	}
 	
 	@GetMapping("/cart")	
-	public ModelAndView orderCurrent(@ModelAttribute("order") Order order, Model model) {
+	public ModelAndView orderCurrent(@ModelAttribute("order") Order order, Errors errors, Model model) {
 		List<Burger> orderedBurgers = new ArrayList<>();
 		order.getCart().stream()
 			.forEach(i -> orderedBurgers.add(bRepo.getBurgerByCodeName(i)));
 		
-		model.addAttribute("orderedBurgers", orderedBurgers);
+		Map<Burger, Integer> orderedBurgersMap = sortBurgers(orderedBurgers);
 		
+		model.addAttribute("orderedBurgers", orderedBurgersMap);
 		return new ModelAndView("order/orderCart.html");
+	}
+	
+	@GetMapping("/cart/delete_burger")	
+	public ModelAndView orderCurrentDelete(@ModelAttribute("order") Order order, Model model,
+			@RequestParam(name="codeName") String codeName, HttpSession session) {
+		
+		List<String> codeNames = new ArrayList<>(); 
+		
+		List<Burger> orderedBurgers = new ArrayList<>();
+		order.getCart().stream()
+			.forEach(i -> orderedBurgers.add(bRepo.getBurgerByCodeName(i)));
+		
+		orderedBurgers.remove(bRepo.getBurgerByCodeName(codeName));
+		
+		orderedBurgers.stream().forEach(i -> codeNames.add(i.getCodeName()));
+		Map<Burger, Integer> orderedBurgersMap = sortBurgers(orderedBurgers);
+		
+		order.setCartFull(codeNames);
+		order.setPrice(calculatePrice(order));
+		model.addAttribute("orderedBurgers", orderedBurgersMap);
+		model.addAttribute("order", order);
+		return new ModelAndView("redirect:/order/cart");
 	}
 	
 	@ModelAttribute
@@ -71,14 +101,16 @@ public class OrderController {
 	}
 	
 	@PostMapping("/cart")
-	 public ModelAndView orderCurrentPost(@ModelAttribute("order") Order order, Model model) {
+	 public ModelAndView orderCurrentPost(@Valid @ModelAttribute("order") Order order, Errors errors, Model model) {
 		ModelAndView modelAndView = new ModelAndView();
-		
-		order.setAddres("Moscow");
-		
+		if (errors.hasErrors()) {
+			modelAndView.setViewName("order/orderCart.html");
+			return modelAndView;
+		}
 		order.setPrice(calculatePrice(order));
 		
 		order.setOrdersDate(LocalDateTime.now());
+		
 		
 		if (model.getAttribute("user")==null) {
 			order.setUser(null);
@@ -92,16 +124,17 @@ public class OrderController {
 			modelAndView.setViewName("redirect:/");
 			
 			return modelAndView;
-		}else {
+		} else {
 			order.setUser((User) model.getAttribute("user"));
-		}
-		log.info(""+order);
-		oRepo.save(order);
-		for (String codeName : order.getCart()) {
-			Reference ref = new Reference();
-			ref.setBurger(bRepo.getBurgerByCodeName(codeName));
-			ref.setOrder(order);
-			rRepo.save(ref);
+			log.info(""+order);
+			oRepo.save(order);
+			for (String codeName : order.getCart()) {
+				Reference ref = new Reference();
+				ref.setBurger(bRepo.getBurgerByCodeName(codeName));
+				ref.setOrder(order);
+				rRepo.save(ref);
+			}
+		
 		}
 		modelAndView.setViewName("redirect:/lobby");
 		
@@ -111,12 +144,22 @@ public class OrderController {
 	@PostMapping()
 	 public ModelAndView OrderPost(@ModelAttribute("order") Order order, Model model) {		
 		order.setPrice(calculatePrice(order));
-		log.info(""+order);
+		
 		return new ModelAndView("redirect:/order/cart");
 	 }
 	
+	//Methods
 	public double calculatePrice(Order order) {
 		return order.getCart().stream().mapToDouble(i-> bRepo.getBurgerByCodeName(i).getPrice()).sum();
+	}
+	
+	public Map<Burger, Integer> sortBurgers(List<Burger> orderedBurgers) {
+		Map<Burger, Integer> res = new HashMap<>(); 
+		
+		orderedBurgers.stream().forEach(i -> res.put(i, res.containsKey(i)? 
+				res.get(i)+1 : 1));
+		
+		return res;
 	}
 	
 
